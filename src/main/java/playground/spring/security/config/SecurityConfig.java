795aedc1e95a23d029f106a435de5b059bedf084
panos <N!p21b11!M>
2022-06-06 21:26:47 +0300

    multi config

--- src/main/java/playground/spring/security/config/SecurityConfig.java
+++ src/main/java/playground/spring/security/config/SecurityConfig.java
@@ -1,12 +1,28 @@
 package playground.spring.security.config;
 
+import javax.annotation.Resource;
+
+import org.keycloak.adapters.springsecurity.KeycloakConfiguration;
+import org.keycloak.adapters.springsecurity.KeycloakSecurityComponents;
+import org.keycloak.adapters.springsecurity.authentication.KeycloakAuthenticationProvider;
+import org.keycloak.adapters.springsecurity.config.KeycloakWebSecurityConfigurerAdapter;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.FilterType;
+import org.springframework.core.annotation.Order;
+import org.springframework.http.HttpMethod;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.security.core.authority.mapping.SimpleAuthorityMapper;
+import org.springframework.security.core.session.SessionRegistryImpl;
+import org.springframework.security.web.authentication.session.RegisterSessionAuthenticationStrategy;
+import org.springframework.security.web.authentication.session.SessionAuthenticationStrategy;
 import playground.spring.security.providers.ApiKeyAuthenticationFilter;
 import playground.spring.security.providers.ApiKeyAuthenticationProvider;
 import playground.spring.security.providers.UsernamePasswordAuthenticationProvider;
@@ -14,33 +30,9 @@ import playground.spring.security.providers.UsernamePasswordAuthenticationProvid
 /**
  * @author Panos Bariamis (pbaris)
  */
-@Configuration
-@EnableWebSecurity
-public class SecurityConfig extends WebSecurityConfigurerAdapter {
+//@Configuration
+//@EnableWebSecurity
 
-    @Autowired
-    private UsernamePasswordAuthenticationProvider usernamePasswordAuthenticationProvider;
+public class SecurityConfig /*extends GlobalMethodSecurityConfiguration*/ {
 
-    @Autowired
-    private ApiKeyAuthenticationProvider apiKeyAuthenticationProvider;
-
-    @Override
-    protected void configure(final AuthenticationManagerBuilder auth) throws Exception {
-        auth.authenticationProvider(usernamePasswordAuthenticationProvider)
-            .authenticationProvider(apiKeyAuthenticationProvider);
-    }
-
-    @Override
-    protected void configure(HttpSecurity http) throws Exception {
-        http.csrf().disable()
-            .httpBasic()
-            .and()
-            .sessionManagement().sessionCreationPolicy(SessionCreationPolicy.STATELESS)
-            .and()
-            .addFilter(new ApiKeyAuthenticationFilter(authenticationManager()))
-            .authorizeRequests()
-            .anyRequest()
-            .authenticated()
-        ;
-    }
 }
--- src/main/java/playground/spring/security/controllers/TestStoreFrontController.java
+++ src/main/java/playground/spring/security/controllers/TestStoreFrontController.java
@@ -1,7 +1,9 @@
 package playground.spring.security.controllers;
 
 import java.util.Map;
+import javax.annotation.security.RolesAllowed;
 
+import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -10,10 +12,12 @@ import org.springframework.web.bind.annotation.RestController;
  * @author Panos Bariamis (pbaris)
  */
 @RestController
-@RequestMapping("/test")
-public class TestController {
+@RequestMapping("/api-sf")
+public class TestStoreFrontController {
 
-    @GetMapping
+//    @RolesAllowed("VIEW")
+    @PreAuthorize("permitAll()")
+    @GetMapping("/shell")
     public Map<String, String> test() {
         return Map.of(
             "name", "Test",
